342c56670b5a849335089f1057f28a260b19f86d
Oyot Onyenwe <oyotonyenwe@gmail.com>
2020-10-15 12:58:54 -0400

    rectangle

---
+++ python/rectangle.py
@@ -0,0 +1,8 @@
+length = int(input("Enter length of a rectangle. "))
+width = int(input("Enter the width of a rectangle. "))
+
+area = length * width
+perimeter = 2 * length + 2 * width
+
+print("Area = " + str(area) + " units^2")
+print("Perimeter = " + str(perimeter) + " units")
