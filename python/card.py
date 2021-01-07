7b9f6909e11e645eeb93fc3cc3f04e1267d9e19c
Oyot Onyenwe <oyotonyenwe@gmail.com>
2021-01-07 09:55:52 -0500

    card calc

---
+++ python/card.py
@@ -0,0 +1,28 @@
+
+import random
+
+card1 = random.randint(0,52)
+card2 = random.randint(0,52)
+card3 = random.randint(0,52)
+card4 = random.randint(0,52)
+card5 = random.randint(0,52)
+
+if card1 == card2 or card3 or card4 or card5:
+    
+    print('equal')
+elif card2 == card1 or card3 or card4 or card5:
+
+    print('equal')
+elif card3 ==
+else:
+
+    print("The first card is:", card1)
+    print("The second card is:", card2)
+    print("The third card is:", card3)
+    print("The fourth card is:", card4)
+    print("The fifth card is:", card5)
+
+
+
+
+
