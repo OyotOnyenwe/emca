32b3b5714ec0fa425b7bf81b13c96cba29c1f564
Oyot Onyenwe <oyotonyenwe@gmail.com>
2021-01-08 20:03:05 -0500

    undo

--- python/card.py
+++ python/card.py
@@ -13,14 +13,9 @@ suit3 = random.choice(('hearts','diamonds','clubs','spades'))
 suit4 = random.choice(('hearts','diamonds','clubs','spades'))
 suit5 = random.choice(('hearts','diamonds','clubs','spades'))
 
-card1 = 'the 1st card is the', number1, 'of', suit1
-card2 = 'the 2nd card is the', number2, 'of', suit2
-card3 = 'the 3rd card is the', number3, 'of', suit3
-card4 = 'the 4th card is the', number4, 'of', suit4
-card5 = 'the 5th card is the', number5, 'of', suit5
+print('the 1st card is the', number1, 'of', suit1)
+print('the 2nd card is the', number2, 'of', suit2)
+print('the 3rd card is the', number3, 'of', suit3)
+print('the 4th card is the', number4, 'of', suit4)
+print('the 5th card is the', number5, 'of', suit5)
 
-print (card1)
-print (card2)
-print (card3) 
-print (card4)
-print (card5)
