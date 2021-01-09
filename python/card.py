ca859d93c0a20c3d87e972c3fb62e1903136bcb0
Oyot Onyenwe <oyotonyenwe@gmail.com>
2021-01-08 20:18:45 -0500

    if statement

--- python/card.py
+++ python/card.py
@@ -1,11 +1,11 @@
 
 import random
 
-number1 = random.choice(('ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'))
-number2 = random.choice(('ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'))
-number3 = random.choice(('ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'))
-number4 = random.choice(('ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'))
-number5 = random.choice(('ace',2,3,4,5,6,7,8,9,10,'jack','queen','king'))
+number1 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+number2 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+number3 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+number4 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+number5 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
 
 suit1 = random.choice(('hearts','diamonds','clubs','spades'))
 suit2 = random.choice(('hearts','diamonds','clubs','spades'))
@@ -13,9 +13,35 @@ suit3 = random.choice(('hearts','diamonds','clubs','spades'))
 suit4 = random.choice(('hearts','diamonds','clubs','spades'))
 suit5 = random.choice(('hearts','diamonds','clubs','spades'))
 
-print('the 1st card is the', number1, 'of', suit1)
-print('the 2nd card is the', number2, 'of', suit2)
-print('the 3rd card is the', number3, 'of', suit3)
-print('the 4th card is the', number4, 'of', suit4)
-print('the 5th card is the', number5, 'of', suit5)
+card1 = number1, suit1
+card2 = number2, suit2
+card3 = number3, suit3
+card4 = number4, suit4
+card5 = number5, suit5
 
+if card1 == card2 or card1 == card3 or card1 == card4 or card1 == card5 or card2 == card3 or card2 == card4 or card2 == card5 or card3 == card4 or card3 == card5 or card4 == card5:
+    print('Error, regenerating')
+    
+    number1 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+    number2 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+    number3 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+    number4 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+    number5 = random.choice(('ace  ','2    ','3    ','4    ','5    ','6    ','7    ','8    ','9    ','10    ','jack ','queen','king '))
+
+    suit1 = random.choice(('hearts','diamonds','clubs','spades'))
+    suit2 = random.choice(('hearts','diamonds','clubs','spades'))
+    suit3 = random.choice(('hearts','diamonds','clubs','spades'))
+    suit4 = random.choice(('hearts','diamonds','clubs','spades'))
+    suit5 = random.choice(('hearts','diamonds','clubs','spades'))
+
+    print(number1, 'of', suit1)
+    print(number2, 'of', suit2)
+    print(number3, 'of', suit3)
+    print(number4, 'of', suit4)
+    print(number5, 'of', suit5)
+else:
+    print(number1, 'of', suit1)
+    print(number2, 'of', suit2)
+    print(number3, 'of', suit3)
+    print(number4, 'of', suit4)
+    print(number5, 'of', suit5)
