e1913d073061f231f56eeb1911633f2b8abb28a7
Oyot Onyenwe <oyotonyenwe@gmail.com>
2021-01-13 22:08:26 -0500

    alternate card generator

---
+++ python/altcard.py
@@ -0,0 +1,14 @@
+
+import random
+
+card1 = random.randint(1,52)
+card2 = random.randint(1,52)
+card3 = random.randint(1,52)
+card4 = random.randint(1,52)
+card5 = random.randint(1,52)
+
+print(card1)
+print(card2)
+print(card3)
+print(card4)
+print(card5)
